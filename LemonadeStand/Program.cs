d94e1a4f77afe4b1dee347e8021ecf060affc140
Serdar Yagci <srdrygc@gmail.com>
2018-08-15 13:29:37 -0500

    asked user number of cups, price per cup,cost per cup and calculated and displayedtotal revenue, total expense and total profit for each location.

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -7,7 +7,33 @@ namespace LemonadeStand
         static void Main(string[] args)
         {
             Console.WriteLine("How many stands do you want to open?");
-            ont numberOfStands = int.Parse(Console.ReadLine());
+            int numberOfStands = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberOfStands; i++)
+            {
+                LemonadeStand newStand = new LemonadeStand();
+
+                Console.WriteLine("What is the name of location " + i + "?");
+                newStand.Name = Console.ReadLine();
+
+                Console.WriteLine("How many cups will you sell in this location ");
+                newStand.NumberOfCups = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("What is the price in this location");
+                newStand.PricePerCup = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("What is the cost in this location");
+
+                newStand.CostPerCup = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("This stand's total revenue will be "
+                    + newStand.GetTotalRevenue());
+                Console.WriteLine("This stand's total expense will be "
+                    + newStand.GetTotalExpenses());
+
+                Console.WriteLine("This stand's total profit will be "
+                    + newStand.GetTotalProfit());
+            }
         }
     }
 }
